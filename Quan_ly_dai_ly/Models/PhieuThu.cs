6ec8146186d6467455bedbbe024266f9a36c8be3
tridominh-cloud <minhtri2416@gmail.com>
2025-09-17 20:54:41 +0700

    commit

---
+++ Quan_ly_dai_ly/Models/PhieuThu.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Quan_ly_dai_ly.Models;
+
+public class PhieuThu
+{
+    [Key]
+    public int MaPhieuThu { get; set; } = 0;
+    public int MaDaiLy { get; set; } = 0;
+    public DateTime NgayThuTien { get; set; } = DateTime.Now;
+    public long SoTienThu { get; set; } = 0;
+
+    public DaiLy DaiLy { get; set; } = null!;
+}
